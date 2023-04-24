068c46342a11af287e2dfd1a7226a4d7cd079867
Andrew Wang <Andrewruujin@gmail.com>
2023-04-23 20:41:33 -0400

    commit tree 07eac168d8900ff546ea0193fba660096afd0387

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,10 +1,35 @@
 class PlanetsController < ApplicationController
-  before_action :set_planet, only: %i[ show update destroy ]
 
-  # GET /planets
   def index
-    planets = Planet.all
-
-    render json: planets
+    render json: Planet.all
   end
+  
+  def show
+    planet = find_planet
+    render json: planet, serializer: ScientistPlanetSerializer
+  end
+
+  def create
+    planet = Planet.create!(planet_params)
+    render json: planet, status: :created
+  end
+
+  private
+  
+    def planet_params
+      params.permit(:name, :age)
+    end
+    
+    def find_planet
+      Planet.find(params[:id])
+    end
+  
+    def render_not_found_response
+      render json: { error: "Planet not found" }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
